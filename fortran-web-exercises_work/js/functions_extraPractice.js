b0ff44b5a98d76cacd4b4a258cc3d71cf735224b
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-06 09:03:39 -0600

    corrects #4 and completes #5

--- fortran-web-exercises_work/js/functions_extraPractice.js
+++ fortran-web-exercises_work/js/functions_extraPractice.js
@@ -31,12 +31,21 @@
 // identity(-4);
 // #4 Make a function named isFive(input)
 function isFive(input) {
-        console.log(input+5);
+        if(input === 5){
+                console.log("This input is 5");
+        } else {
+                console.log("This input is not 5");
+        }
 }
 isFive(5);
 isFive(-4);
-// Make a function named addFive(input) that adds five to some input.
-//     Make a function named isMultipleOfFive(input)
+// #5 Make a function named addFive(input) that adds five to some input.
+// function addFive(input) {
+//         console.log(input+5);
+// }
+// addFive(5);
+// addFive(-4);
+// Make a function named isMultipleOfFive(input)
 // Make a function named isThree(input)
 // Make a function named isMultipleOfThree(input)
 // Make a function named isMultipleOfThreeAndFive(input)
