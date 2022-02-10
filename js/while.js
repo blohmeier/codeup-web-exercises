7390d207affd70bdcd6a4415111de69c692e82c1
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-10 09:24:51 -0600

    updats while.js to clarify accepted solution

--- js/while.js
+++ js/while.js
@@ -6,36 +6,22 @@
 //     n++;
 // }
 //"Do While Loop" #1.
-// let allCones = Math.floor(Math.random() * 100) + 50;
-// console.log(allCones);
-// let conesBeingBought;
-// let conesSoldTest = (allCones < conesBeingBought)
-// do {
-//     conesBeingBought = Math.floor(Math.random() * 5) + 1;
-//     console.log(conesBeingBought);
-//     if (allCones > conesBeingBought) {
-//         console.log(conesBeingBought + " cones sold...");
-//         allCones = allCones - conesBeingBought;
-//     } else {
-//         console.log("Cannot sell you " + conesBeingBought + " cones I only have " + allCones + "...");
-//     }
-// } while (!conesSoldTest)
-// console.log("Yay! I sold them all!");
-// var allCones = Math.floor(Math.random() * 100) + 50;
-// do {
-//     let conesSold = Math.floor(Math.random() * 5) + 1;
-//     //console.log("starting allCones value is: " + allCones);
-//     //console.log("starting conesBought value is: " + conesSold);
-//
-//     if ((allCones - conesSold) < 0) {
-//         console.log("Cannot sell you " + conesSold + " cones I only have " + allCones + "...")
-//     } else {
-//
-//         allCones = allCones - conesSold;
-//         console.log(conesSold + " cones sold...")
-//         //console.log("conesSold is now: " + conesSold);
-//     }
-//     //console.log(allCones);
-// }
-// while (allCones > 0);
-// console.log("Yay! I sold them all!");
+
+var allCones = Math.floor(Math.random() * 100) + 50;
+do {
+    let conesSold = Math.floor(Math.random() * 5) + 1;
+    //console.log("starting allCones value is: " + allCones);
+    //console.log("starting conesBought value is: " + conesSold);
+
+    if ((allCones - conesSold) < 0) {
+        console.log("Cannot sell you " + conesSold + " cones I only have " + allCones + "...")
+    } else {
+
+        allCones = allCones - conesSold;
+        console.log(conesSold + " cones sold...")
+        //console.log("conesSold is now: " + conesSold);
+    }
+    //console.log(allCones);
+}
+while (allCones > 0);
+console.log("Yay! I sold them all!");
