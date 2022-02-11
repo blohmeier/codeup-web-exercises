09467a199f17a4473023cfac5627314da19b33c1
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-11 10:57:41 -0600

    starts forEach in 'arrays-lec-fortran.js'

--- js/arrays-lec-fortran.js
+++ js/arrays-lec-fortran.js
@@ -7,8 +7,10 @@
 // var dog3 = "Fifi";
 // var dog4 = "Lola";
 
+
+//Fido is the first element (i.e., an odd element), even though the index is 0.
 var dogNames = ["Fido", "Spot", "Fifi", "Lola"];
-// var dogAges = [5, 2, 3, 10];
+var dogAges = [5, 2, 3, 10];
 // Will not be able to do the below in Java - can't do heterogeneous arrays in Java. Don't do the below.
 // var dogs = ["Fido", 5, "Spot", 2];
 
@@ -33,16 +35,30 @@ console.log(dogNames.length);
 // log the 2nd to last element
 // console.log(dogNames[dogNames.length - 2]);
 
-// loops and arrays are like peas and carrots
-for (var i = 0; i < dogNames.length; i++) { // iterates 4 times.
-    // output the ith element in the array
-    // or
-    // output the element sub i
-    // or
-    // output the element at index i
-    console.log("Dog at index " + i + " is " + dogNames[i]);
-}
+// // loops and arrays are like peas and carrots
+// for (var i = 0; i < dogNames.length; i++) { // iterates 4 times.
+//     // output the ith element in the array
+//     // or
+//     // output the element sub i
+//     // or
+//     // output the element at index i
+//     console.log("Dog at index " + i + " is " + dogNames[i]);
+// }
+//
+// // reverse the output by reversing the index counter
+// for (let i = dogNames.length - 1; i >= 0; i--) {
+//     console.log("Dog at index " + i + " is " + dogNames[i]);
+// }
+//
+// // output only the odd dogs.
+// // Fido is the first element, even though the index is 0.
+// // therefore I consider Fido to be an odd element
+// // thus even index values will be considered an odd element
+// for (let i = dogNames.length - 1; i >= 0; i--) {
+//     if(i % 2 !== 0) {
+//         console.log("The odd dog at index " + i + " is " + dogNames[i]);
+//     }
+// }
 
-for (let i = dogNames.length - 1; i >= 0; i--) {
-    console.log("Dog at index " + i + " is " + dogNames[i]);
-}
+// forEach loops
+dogNames.forEach()
