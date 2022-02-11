55e278668b80894b70d481bfea09612e14aaaba9
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-11 10:28:25 -0600

    updates 'arrays-lec-fortran.js'

--- js/arrays-lec-fortran.js
+++ js/arrays-lec-fortran.js
@@ -8,5 +8,14 @@
 // var dog4 = "Lola";
 
 var dogNames = ["Fido", "Spot", "Fifi", "Lola"];
-console.log("Dog names: " + dogNames);
+var dogAges = [5, 2, 3, 10];
+// Will not be able to do the below in Java - can't do heterogeneous arrays in Java. Don't do the below.
+var dogs = ["Fido", 5, "Spot", 2];
 
+console.log("Dog names: " + dogNames);
+console.log(dogNames.length);
+
+// first elemnt is at index 0
+console.log(dogNames[0]); // Fido
+// last element is at index length - 1
+console.log(dogNames[dogNames.length-1]); // Lola
