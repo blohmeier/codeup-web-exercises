086425adb221b9aa1518782615c884bfd79e1afb
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-14 11:52:00 -0600

    adds objects-lec_14Feb.js

---
+++ js/objects-lec_14Feb.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// CREATING AN OBJECT
+
+let car = {};
+console.log(typeof car);
+
+car.make = `Ford`;
+
+console.log(car);
+
+car.model = `Mustang`;
+
+console.log(car.model);
+
+
+// ASSIGNING PROPERTIES TO AN OBJECT
