a810ffda93e54e77254a3ee3da703a816085e126
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-15 09:31:57 -0600

    finishes 'TODOs' in

--- js/objects.js
+++ js/objects.js
@@ -144,6 +144,15 @@
      *      ---
      *      ...
      */
+    console.log(books.length);
+
+    for (let i = 0; i <= (books.length - 1); i++) {
+        //console.log("Book # " + Object.keys(books[i]));
+        console.log("Book # " + (i + 1));
+        console.log("Title : " + books[i].title);
+        console.log("Author : " + books[i].author.firstName + " " + books[i].author.lastName);
+    }
+
 
     /**
      * Bonus:
