0beda1605ac43870f4a0dababcc3216a737c2acc
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-16 09:15:05 -0600

    initial commit of 'objects-lec_16Feb'

---
+++ js/objects-lec_16Feb.js
@@ -0,0 +1,29 @@
+/*
+JS Object Warmup, pt 1:
+Create an object representing a person. It should have properties representing:
+names (an array of strings)
+date of birth
+occupation
+Hard code some values in that object, then log it and its properties to the console*/
+var person = [
+    {
+        name: "Tommy Johnson",
+        dob: Date(1995, 11, 17),
+        occupation: "doctor"
+    },
+    {
+        name: "Tommy Johnson",
+        dob: Date(1995, 11, 17),
+        occupation: "doctor"
+    },
+    {
+        name: "Tommy Johnson",
+        dob: Date(1995, 11, 17),
+        occupation: "doctor"
+    },
+    {
+        name: "Tommy Johnson",
+        dob: Date(1995, 11, 17),
+        occupation: "doctor"
+    },
+];
