a0d4c06b02ca3c7b8a94ca98d7ea75f89d63fc7a
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-16 10:38:25 -0600

    coompletes part3 of 'objects-lec_16Feb'

--- js/objects-lec_16Feb.js
+++ js/objects-lec_16Feb.js
@@ -106,4 +106,54 @@ Create a property on the person object to store their contact info
     -> Assign the contact info variable to that property
     -> Log that entire object and its properties to the console*/
 
+// Failed attempt:
+/*
+function ConotactInfo(phoneNumber, streetAddress, city, state, postalCode\, emailAddress) {
+    this.names = names;
+    this.occupation = occupation;
+    this.dob = dob;
+}
 
+function Person(names, occupation, dob) {
+    this.names = names;
+    this.occupation = occupation;
+    this.dob = dob;
+}
+
+let person2 = new Person(
+    names: ["Casey", "J", "Edwards"],
+    occupation:
+dob
+));
+console.log(person2.contactInfo.emailAddress);*/
+//Copied over from slack:
+// Defines a contact info object
+function ContactInfo(phoneNumber, streetAddress, city, state, postalCode, emailAddress){
+    this.phoneNumber = phoneNumber;
+    this.streetAddress = streetAddress;
+    this.city = city;
+    this.state = state;
+    this.postalCode = postalCode;
+    this.emailAddress = emailAddress;
+}
+// Defines a person object
+function Person(names, occupation, dob, contactInfo) {
+    this.names = names;
+    this.occupation = occupation;
+    this.dob = dob;
+    this.contactInfo = contactInfo;
+}
+// makes a new person object instance (by invoking the constructor)
+let person2 = new Person(
+    ["Casey", "J", "Edwards"],
+    "Cat Herder / Firefighter",
+    "11.30.1989",
+    new ContactInfo(
+        '123.456.7890',
+        '123 Anywhere Ln',
+        'Beverly Hills',
+        'CA',
+        '90210',
+        'nope@no-no-no.no'));
+
+console.log(person2.contactInfo.emailAddress);
