b096dd8242634c7e8a8d25ea4af0ceee4b3b3a0b
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-17 09:23:24 -0600

    adds warmup feb 17

---
+++ warmup_17Feb.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/*
+JS Object Warmup, pt 4:
+We are going to create a method which
+allows a user to input their desired amount of names names
+use prompt, alert, confirm inside of a while loop
+    assign each name to an array
+once the user decides they are done giving names, end the loop and return the string array
+then assigning that returned array by invoking your new function on the Person.names property value!
+    Things to consider:
+    Should this be a standalone function or should it be a method on the Person object?*/
+
+
