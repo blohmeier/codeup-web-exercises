fd9418ec2e26c36560438b9cfd0b64e6e5fd631a
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-18 12:01:36 -0600

    completes 'selectors.css' and 'css_selectors.html' for in-class upload

--- js/warmup_18Feb.js
+++ js/warmup_18Feb.js
@@ -6,7 +6,32 @@ function trueTest (input) {
         return true;
     }
 }
-
 console.log(trueTest(7));
 console.log(trueTest(false));
 console.log(trueTest([]));
+console.log(trueTest(Infinity));
+console.log(trueTest(NaN));
+
+
+/*//correction 1
+function isANumber(maybeANum){
+    return !isNaN(maybeANum);
+}*/
+/*//correction 2
+function isANumber(maybeANum){
+    return Number.isNaN(maybeANum);
+}*/
+/*//correction 3
+function isANumber(maybeANum){
+    return !!parseFloat(maybeANum);
+}*/
+/*
+//correction 4
+/!*function isANumber(maybeANum){
+    return !Number.isNaN(parseFloat(maybeANum));
+}*!/
+//string
+console.log(isANumber("zebra"));
+
+//numeric string
+console.log(isANumber("12"));*/
