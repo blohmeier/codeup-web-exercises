633a5973414cb469d606c9fd37d8f2458cd1b392
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-28 09:29:33 -0600

    adds warmup exercise in class review

--- js/warmup_28Feb.js
+++ js/warmup_28Feb.js
@@ -15,17 +15,33 @@ console.log(removeAll(bugs, "ant"));
   'type error'
 ]
  */
-function removeAll (array1,val) {
+//my solution
+/*function removeAll (array1,val) {
     for(var i = 0; i < array1.length; i++) {
         if( array1[i] === val ) {
 
             array1.splice(i,1);
             i--;
-            /*console.log(removed);
-            console.log(array1);*/
+            /!*console.log(removed);
+            console.log(array1);*!/
         }
     }
     return array1;
+}*/
+
+//in class solution
+function removeAll (anArr, valueToRemove) {
+    let filteredArr = [];
+    anArr.forEach(function (ele) {
+        if (valueToRemove !== ele) {
+            filteredArr.push(ele);
+        }
+    })
+    /*
+    alternative
+     */
+    return filteredArr;
 }
+
 var bugs = ["mosquito", "ant", "scorpion", "ant", "ant", "mosquito", "typo", "reference error", "type error"];
 console.log(removeAll(bugs, "ant"));
