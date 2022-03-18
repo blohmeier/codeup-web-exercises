52df584abec2365b8c97022b9bd62a2571d3a4c8
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-03-18 12:04:57 -0500

    updates 'weather_map_Fri18Mar.html' after consolidated js - map and forcast still working independently

--- js/mapbox_utils.js
+++ js/mapbox_utils.js
@@ -1,3 +1,4 @@
+"use strict";
 /**
  * Define all the file-scoped variables we'll need in various parts of this js file
  * **/
@@ -86,4 +87,65 @@ function setGeocoderEventListener() {
     });
 }
 
+/*ADDED FROM html*/
+let startingLat = 32.7767;
+let startingLon = -96.7970;
 
+getWeatherData(startingLat, startingLon);
+
+function getWeatherData(lat, lon) {
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${TEST1_KEY}`)
+        .then(response => response.json())
+        .then(data => {
+            $('#weather').html(buildCardCont(data.daily));
+        });
+}
+
+function extractWeatherData(dayObj) {
+    return {
+        date: dayObj.dt,
+        dailyTemp: dayObj.temp.day,
+        humidity: dayObj.humidity,
+        pressure: dayObj.pressure
+    }
+}
+
+function buildCardCont(dayArr) {
+    let html = '<div class="row">'
+    for (let i = 0; i < 5; i++) {
+        html += buildWeatherCard(dayArr[i]);
+    }
+    html += '</div>';
+    return html;
+}
+
+function buildWeatherCard(day) {
+    let html = '';
+    let weather = extractWeatherData(day);
+    let formattedDate = formatDate(weather.date);
+    // language=HTML
+    html += `
+            <div class="card" style="width: 18rem;">
+                <div class="card-header">
+                    ${formattedDate}
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Temp: ${weather.dailyTemp}</li>
+                    <li class="list-group-item">Hum: ${weather.humidity}</li>
+                    <li class="list-group-item">Pressure: ${weather.pressure}</li>
+                </ul>
+            </div>`
+    return html;
+}
+
+function formatDate(unixDate) {
+    return new Date(unixDate * 1000).toISOString().split('T')[0];
+}
+
+
+$('#submit').click(function (e) {
+    e.preventDefault();
+    let userLat = $('#lat').val();
+    let userLon = $('#lon').val();
+    getWeatherData(userLat, userLon);
+})
